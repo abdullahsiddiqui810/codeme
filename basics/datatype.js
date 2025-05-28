193514a0b57896792cea973648061e05434a92e7
abdullahsiddiqui810 <abdullahsiddiqui810@gmail.com>
2025-05-28 00:50:28 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,19 @@
+ 
+let name = "abd"
+let age = 10
+let userLogin = true
+
+
+
+
+
+ // number => 2 to power 53
+ // string => ""
+ // bigint
+ // boolean => true/false
+ // null => standalone value 
+ // undefind =>
+ // symbol => unique   
+
+ console.log(typeof undefind);
+ 
