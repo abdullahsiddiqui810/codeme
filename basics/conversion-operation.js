cb521d3df7b711b246649a1d453694f341692ad9
abdullahsiddiqui810 <abdullahsiddiqui810@gmail.com>
2025-05-28 17:53:56 +0000

    conversion

---
+++ basics/conversion-operation.js
@@ -0,0 +1,31 @@
+let score = false
+
+// console.log(typeof score)
+
+let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abd" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// true => 1; false =>0
+// "" => false 
+// "abc" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
