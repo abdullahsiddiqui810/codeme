10408a97026c9ed35a7c9de0372823ee3ac3924b
abdullahsiddiqui810 <abdullahsiddiqui810@gmail.com>
2025-07-02 15:34:47 +0000

    add file

---
+++ basics/iife.js
@@ -0,0 +1,10 @@
+//IIFE
+
+( function code (){
+    // name IIFE
+  console.log('DB Connuted')
+}) ();
+
+( () =>{
+   console.log('DB Connuted ${abd}') ;
+})('abd')
---
+++ basics/one.js
@@ -0,0 +1,8 @@
+const isUserloggedIn = true
+const temperature = 41
+
+if (temperature === 41){
+  console.log('less than 50')
+}else{
+    console.log('temperature is greater than 50')
+}
